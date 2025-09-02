4cd7e8630b484cb85f7c5900fec65c76d383086f
Manuel Geiger <40306539+Ectras@users.noreply.github.com>
2025-09-02 20:32:27 +0200

    Add wrap() to convert complex slice to approx_eq wrapper

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,10 @@
+use bytemuck::TransparentWrapper;
 use float_cmp::{ApproxEq, F64Margin};
 use num_complex::Complex64;
 
 /// A transparent wrapper around a [`Complex64`] to provide it with an [`ApproxEq`]
 /// implementation.
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, TransparentWrapper)]
 #[repr(transparent)]
 pub struct Complex64ApproxEq(Complex64);
 
@@ -14,4 +15,11 @@ impl ApproxEq for Complex64ApproxEq {
         let margin = margin.into();
         self.0.re.approx_eq(other.0.re, margin) && self.0.im.approx_eq(other.0.im, margin)
     }
+}
+
+#[cfg(test)]
+/// Converts the slice to a slice of the transparent Complex64 wrapper type that
+/// implements [`ApproxEq`].
+pub fn wrap(data: &[Complex64]) -> &[Complex64ApproxEq] {
+    Complex64ApproxEq::wrap_slice(data)
 }
